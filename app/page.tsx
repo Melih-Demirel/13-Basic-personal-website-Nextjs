e10cebfebdbc1fea5d39bc377573291034286ee7
Melih <melih.demirel@student.uhasselt.be>
2024-04-16 14:19:17 +0200

    a

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,6 @@ export default function Home() {
     return (
         <>
             <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;500&display=swap" rel="stylesheet" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
